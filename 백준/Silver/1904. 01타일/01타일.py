ad1218f7aaf2d33b1cdb60eeb2b84a8619df60fc
sumin <91295349+WantKakao@users.noreply.github.com>
2023-10-27 00:13:58 +0900

    [Silver III] Title: 01타일, Time: 284 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,8 +1,16 @@
 n = int(input())
-ans = [0 for _ in range(1000001)]
-ans[0] = 0
+# ans = [0 for _ in range(1000001)]
+ans = [0, 0, 0]
+# ans[0] = 0
 ans[1] = 1
 ans[2] = 2
-for i in range(3, 1000001):
-    ans[i] = (ans[i-2] + ans[i-1])%15746
-print(ans[n])
+# for i in range(3, 1000001):
+#     ans[i] = (ans[i-2] + ans[i-1])%15746
+# print(ans[n])
+if n == 1 or n == 2:
+    print(ans[n])
+else:
+    for i in range(n-2):
+        ans[0], ans[1] = ans[1], ans[2]
+        ans[2] = (ans[0] + ans[1]) % 15746
+    print(ans[2])
