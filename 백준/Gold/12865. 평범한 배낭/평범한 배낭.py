f67a8cd8893ec3e43b5eaf919a3520c15b38f3b1
sumin <91295349+WantKakao@users.noreply.github.com>
2023-10-30 17:48:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 268 ms, Memory: 111376 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,16 +1,12 @@
-n, k = map(int, input().split())
+import sys
+n, k = map(int, sys.stdin.readline().split())
 items = [0]
 for _ in range(n):
     items.append([*map(int, input().split())])      # items = [[0], [weight, value], ..., ]
-graph = [[0 for _ in range(k+1)] for _ in range(n+1)]
-for j in range(1, k+1):
-    if j >= items[1][0]:
-        graph[1][j] = items[1][1]
-for i in range(2, n+1):
-    for j in range(1, k+1):
-        if j >= items[i][0]:
-            graph[i][j] = max(graph[i-1][j], graph[i][j-1], graph[i-1][j-items[i][0]]+items[i][1])
-        else:
-            graph[i][j] = max(graph[i - 1][j], graph[i][j - 1])
+dp = [0 for _ in range(k+1)]
 
-print(graph[-1][-1])
+for i in range(1, n+1):
+    for j in range(k, 0, -1):
+        if j >= items[i][0]:
+            dp[j] = max(dp[j], dp[j-items[i][0]]+items[i][1])
+print(dp[-1])
