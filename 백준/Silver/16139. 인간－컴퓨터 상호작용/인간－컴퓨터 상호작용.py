6682d8c2a5df2340e4506eaaa1006317df7d5c0a
sumin <91295349+WantKakao@users.noreply.github.com>
2024-03-01 10:34:30 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 528 ms, Memory: 174616 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,10 +1,20 @@
-word = input()
+import sys
+
+input = sys.stdin.readline
+
+name = input().strip()
 n = int(input())
-for _ in range(n):
-    w, a, b = map(str, input().split())
-    a, b = map(int, (a, b))
-    ans = 0
-    for i in range(a, b+1):
-        if word[i] == w:
-            ans += 1
-    print(ans)
+arr = [[0 for i in range(26)] for i in range(len(name))]
+arr[0][ord(name[0]) - 97] = 1
+for i in range(1, len(name)):
+    arr[i][ord(name[i]) - 97] = 1
+    for j in range(26):
+        arr[i][j] += arr[i - 1][j]
+for i in range(n):
+    a = input().split()
+    if int(a[1]) > 0:
+        res = arr[int(
+            a[2])][ord(a[0]) - 97] - arr[int(a[1]) - 1][ord(a[0]) - 97]
+    else:
+        res = arr[int(a[2])][ord(a[0]) - 97]
+    print(res)
