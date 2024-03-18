898875119d137077e8cea1e0af937acdd77d9fcc
sumin <91295349+WantKakao@users.noreply.github.com>
2024-03-18 14:37:55 +0900

    [Silver V] Title: 삼각형만들기, Time: 72 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
@@ -4,11 +4,8 @@ n = int(input())
 end = (n-1)//2
 start = math.ceil(n/3)
 ans = 0
-if n == 1 or n == 2 or n == 4:
-    print(0)
-else:
-    for i in range(end, start-1, -1):
-        j = i
-        k = n - 2*i
-        ans += (j - k)//2 + 1
-    print(ans)
+for i in range(end, start-1, -1):
+    j = i
+    k = n - 2*i
+    ans += (j - k)//2 + 1
+print(ans)
