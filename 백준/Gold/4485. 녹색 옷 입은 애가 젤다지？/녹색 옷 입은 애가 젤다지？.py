ede7e7143c0fa79ce783351c452e8a94fb659489
sumin <91295349+WantKakao@users.noreply.github.com>
2024-03-19 11:27:52 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 712 ms, Memory: 36160 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 from collections import deque
 input = sys.stdin.readline
 idx = 1
@@ -12,17 +13,20 @@ while 1:
             cave.append([*map(int, input().split())])
         cost = [[1e6 for _ in range(n)] for _ in range(n)]
         cost[0][0] = cave[0][0]
-        q = deque()
-        q.append((0, 0, cave[0][0]))
+        # q = deque()
+        q = [(0, 0, cave[0][0])]
+        # q.append((0, 0, cave[0][0]))
         dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
         while q:
-            x, y, c = q.popleft()
+            # x, y, c = q.popleft()
+            x, y, c = heapq.heappop(q)
             for i in range(4):
                 nx, ny = x + dx[i], y + dy[i]
                 if 0 <= nx < n and 0 <= ny < n:
                     temp = c + cave[nx][ny]
                     if temp < cost[nx][ny]:
-                        q.append((nx, ny, temp))
+                        # q.append((nx, ny, temp))
+                        heapq.heappush(q, (nx, ny, temp))
                         cost[nx][ny] = temp
         print(f'Problem {idx}: {cost[-1][-1]}')
         idx += 1
