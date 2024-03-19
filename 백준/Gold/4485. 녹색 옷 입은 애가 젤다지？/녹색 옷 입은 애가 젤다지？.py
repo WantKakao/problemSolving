7bb92f4d1aff3052b568a3c474077693b8d330bd
sumin <91295349+WantKakao@users.noreply.github.com>
2024-03-19 11:22:41 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 1348 ms, Memory: 222024 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+idx = 1
+while 1:
+    n = int(input())
+    if n == 0:
+        break
+    else:
+        cave = []
+        for _ in range(n):
+            cave.append([*map(int, input().split())])
+        cost = [[1e6 for _ in range(n)] for _ in range(n)]
+        cost[0][0] = cave[0][0]
+        q = deque()
+        q.append((0, 0, cave[0][0]))
+        dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+        while q:
+            x, y, c = q.popleft()
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                if 0 <= nx < n and 0 <= ny < n:
+                    temp = c + cave[nx][ny]
+                    if temp < cost[nx][ny]:
+                        q.append((nx, ny, temp))
+                        cost[nx][ny] = temp
+        print(f'Problem {idx}: {cost[-1][-1]}')
+        idx += 1
