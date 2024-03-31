6220826a93983d75d07bfff98224bc41d68580c6
sumin <91295349+WantKakao@users.noreply.github.com>
2024-03-31 14:03:20 +0900

    [Silver II] Title: 로또, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,17 +1,13 @@
-from itertools import combinations
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 
 while 1:
-    info = [*map(int, input().split())]
-    if info == [0]:
+    nums = [*map(int, input().split())]
+    if len(nums) == 1:
         break
-    else:
-        n, nums = info[0], info[1:]
-        combi = list(combinations(nums, 6))
-        combi.sort()
-        for i in combi:
-            for j in i:
-                print(j, end=' ')
-            print()
+    k = nums.pop(0)
+    comb = list(combinations(nums, 6))
+    for c in comb:
+        print(' '.join(map(str, c)))
     print()
