580a44dd6fdb2e51f7b76a8ca18b8f3f5d2ea124
sumin <91295349+WantKakao@users.noreply.github.com>
2024-04-18 11:53:44 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+skyline = [0]
+high = 0
+ans = 0
+for _ in range(n):
+    x, y = map(int, input().split())
+    if y > high:
+        ans += 1
+        skyline.append(y)
+    else:
+        while skyline:
+            p = skyline.pop()
+            if p <= y:
+                skyline.append(p)
+                break
+        skyline.append(y)
+        if p != y and y != 0:
+            ans += 1
+    high = y
+
+print(ans)
