106f348f784a1f2628cc138258f682ff64ca5d80
sumin <91295349+WantKakao@users.noreply.github.com>
2024-07-23 11:00:57 +0900

    [Platinum IV] Title: XOR 최대, Time: 524 ms, Memory: 215728 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/32073. XOR 최대/XOR 최대.py
+++ 백준/Platinum/32073. XOR 최대/XOR 최대.py
@@ -4,30 +4,39 @@ for _ in range(T):
     N = int(input())
     S = input()
 
-    target = ''
-    for num in S:
-        if num == '1':
-            target += '0'
-        else:
-            target += '1'
-
-    ans = 0
-    for idx in range(N):
-        if ans != 0:
+    idx = -1
+    r_idx = -1
+    for i in range(1, N):
+        if S[i-1] == '1' and S[i] == '0':
+            idx = i
             break
+        if S[i-1] == '0' and S[i] == '1':
+            r_idx = i
 
-        if S[idx] == '0':
-            temp = S[idx:]
-            lt = len(temp)
-            for idx_s in range(idx+1):
-                if S[idx_s] == '1':
-                    decimal = int(S[idx_s:idx_s+lt], 2) ^ int(S, 2)
-                    ans = max(ans, decimal)
+    # 전부 1 이거나 전부 0 일때
+    if idx == r_idx == -1:
+        if S[0] == '0':
+            print(0)
+        else:
+            print('1'*(N-1)+'0')
 
-    if '1' in S:
-        decimal = int('1', 2) ^ int(S, 2)
-        ans = max(ans, decimal)
-    if '0' in S:
-        decimal = int('0', 2) ^ int(S, 2)
-        ans = max(ans, decimal)
-    print(bin(ans)[2:])
+    # 0 에서 1 로 바뀌는 것만 있을때
+    elif idx == -1:
+        ans = 0 ^ int(S, 2)
+        print(bin(ans)[2:])
+
+    else:
+        ans_idx = idx-1
+        comp = S[idx:]
+        l = len(comp)
+        for i in range(1, l):
+            if comp[i] == '1':
+                break
+            else:
+                if ans_idx-1 >= 0 and S[ans_idx-1] == '1':
+                    ans_idx -= 1
+                else:
+                    break
+
+        k = int(S[ans_idx:ans_idx+l], 2) ^ int(S, 2)
+        print(bin(k)[2:])
