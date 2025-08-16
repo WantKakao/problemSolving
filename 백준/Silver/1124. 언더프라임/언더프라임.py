cecd050f960566dec75e7544644d9ca2f6a3c6bf
sumin <91295349+WantKakao@users.noreply.github.com>
2025-08-16 22:16:21 +0900

    [Silver I] Title: 언더프라임, Time: 124 ms, Memory: 35784 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.py
+++ 백준/Silver/1124. 언더프라임/언더프라임.py
@@ -1,23 +1,29 @@
+MAX = 100000
+
+# 최소 소인수 (Smallest Prime Factor)
+spf = [0] * (MAX+1)
+for i in range(2, MAX+1):
+    if spf[i] == 0:  # 소수라면
+        for j in range(i, MAX+1, i):
+            if spf[j] == 0:
+                spf[j] = i
+
+# 각 수의 소인수 개수
+factor_cnt = [0] * (MAX+1)
+for i in range(2, MAX+1):
+    factor_cnt[i] = factor_cnt[i // spf[i]] + 1
+
+# 소수 판별 (에라토스테네스)
+is_prime = [True] * (MAX+1)
+is_prime[0] = is_prime[1] = False
+for i in range(2, int(MAX**0.5)+1):
+    if is_prime[i]:
+        for j in range(i*i, MAX+1, i):
+            is_prime[j] = False
+
 A, B = map(int, input().split())
-
-is_prime = [True for _ in range(50000)]
-is_prime[0], is_prime[1] = False, False
-for n in range(2, 50000):
-    if is_prime[n]:
-        for k in range(n*2, 50000, n):
-            if is_prime[k]:
-                is_prime[k] = False
-
-prime = [i for i in range(50000) if is_prime[i]]
 ans = 0
 for num in range(A, B+1):
-    temp = 0
-    for p in prime:
-        if num < p:
-            break
-        while num % p == 0:
-            temp += 1
-            num //= p
-    if is_prime[temp]:
+    if is_prime[factor_cnt[num]]:
         ans += 1
 print(ans)
