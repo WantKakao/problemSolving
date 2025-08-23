7458268688ca3265c4b78efb29692309d9fd0c49
sumin <91295349+WantKakao@users.noreply.github.com>
2025-08-23 09:17:36 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 296 ms, Memory: 49576 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,42 +1,32 @@
 import sys
 input = sys.stdin.readline
-import heapq
 
-v, e = map(int, input().split())
-edge = []
-ans = 0
-parent = [i for i in range(v+1)]
+def find(x):
+    while parent[x] != x:
+        parent[x] = parent[parent[x]]  # 경로 압축
+        x = parent[x]
+    return x
 
-for _ in range(e):
-    i, j, c = map(int, input().split())
-    heapq.heappush(edge, (c, i, j))
-
-
-def find_parent(x):
-    if parent[x] == x:
-        return x
-    else:
-        parent[x] = find_parent(parent[x])
-        return parent[x]
-
-
-def union(x, y):
-    a = find_parent(x)
-    b = find_parent(y)
-    if a > b:
-        parent[a] = b
-    else:
+def union(a, b):
+    a, b = find(a), find(b)
+    if a < b:
         parent[b] = a
-
-
-while edge:
-    c, i, j = heapq.heappop(edge)
-    find_parent(i)
-    find_parent(j)
-    if parent[i] == parent[j]:
-        continue
     else:
-        union(i, j)
-        ans += c
+        parent[a] = b
 
-print(ans)
+V, E = map(int, input().split())
+edges = []
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    edges.append((w, u, v))
+
+edges.sort()
+parent = [i for i in range(V+1)]
+
+mst_cost = 0
+for w, u, v in edges:
+    if find(u) != find(v):
+        union(u, v)
+        mst_cost += w
+
+print(mst_cost)
