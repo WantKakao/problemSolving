8b8b509053baf2cb3e1312633c982831fc46d285
sumin <91295349+WantKakao@users.noreply.github.com>
2025-09-03 10:05:50 +0900

    [Gold V] Title: 단어 만들기, Time: 1236 ms, Memory: 95436 KB -BaekjoonHub

--- 백준/Gold/1148. 단어 만들기/단어 만들기.py
+++ 백준/Gold/1148. 단어 만들기/단어 만들기.py
@@ -6,43 +6,40 @@ while True:
     S = input().strip()
     if S == '-':
         break
-    cnt = [0]*26
+    temp = [0 for _ in range(26)]
     for s in S:
-        cnt[ord(s)-65] += 1
-    dictionary.append(cnt)
+        temp[ord(s)-65] += 1
+    dictionary.append(temp)
+
+def check_alphabet(word):
+    for i in range(26):
+        if word[i] > temp[i]:
+            return
+    for idx in alphabet_idx:
+        if word[idx] > 0:
+            count[idx] += 1
+    return
 
 while True:
     B = input().strip()
     if B == '#':
         break
-    bcnt = [0]*26
+    temp = [0 for _ in range(26)]
     for b in B:
-        bcnt[ord(b)-65] += 1
-    alphabet_idx = [i for i,v in enumerate(bcnt) if v>0]
-
-    count = [0]*26
-    # dictionary 전체를 한 번만 순회
+        temp[ord(b)-65] += 1
+    alphabet_idx = [i for i, v in enumerate(temp) if v > 0]
+    count = [0 for _ in range(26)]
     for d in dictionary:
-        ok = True
-        for i in range(26):
-            if d[i] > bcnt[i]:
-                ok = False
-                break
-        if not ok: 
-            continue
-        # d 단어가 가능하면, 포함된 알파벳에 대해 카운트
-        for i in alphabet_idx:
-            if d[i] > 0:
-                count[i] += 1
-
-    c_count = [count[i] for i in alphabet_idx]
+        check_alphabet(d)
+    c_count = [v for i, v in enumerate(count) if i in alphabet_idx]
     m, M = min(c_count), max(c_count)
-
-    for i in alphabet_idx:
-        if count[i] == m:
+    for i in range(26):
+        if count[i] == m and i in alphabet_idx:
             print(chr(i+65), end='')
-    print(f' {m} ', end='')
-    for i in alphabet_idx:
-        if count[i] == M:
+    print(' ', end='')
+    print(m, end=' ')
+    for i in range(26):
+        if count[i] == M and i in alphabet_idx:
             print(chr(i+65), end='')
-    print(f' {M}')
+    print(' ', end='')
+    print(M)
