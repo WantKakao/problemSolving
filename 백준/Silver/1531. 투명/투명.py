b3efdb82231b7839e6120be1712b008739a2db60
sumin <91295349+WantKakao@users.noreply.github.com>
2025-09-30 09:06:37 +0900

    [Silver V] Title: 투명, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1531. 투명/투명.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+paper = [[0 for _ in range(100)] for _ in range(100)]
+for _ in range(n):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1-1, x2):
+        for j in range(y1-1, y2):
+            paper[i][j] += 1
+            
+ans = 0
+for i in range(100):
+    for j in range(100):
+        if paper[i][j] > m:
+            ans += 1
+print(ans)
