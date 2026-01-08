689e55be5890ea3cbd9fd7f80f0f85b487b7e7c0
sumin <91295349+WantKakao@users.noreply.github.com>
2026-01-08 12:52:13 +0900

    [Gold IV] Title: 미로만들기, Time: 64 ms, Memory: 35116 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,36 +1,34 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
+input = sys.stdin.readline
 
 n = int(input())
-graph = []
-for _ in range(n):
-    graph.append([*map(int, input().rstrip())])
-INF = 10e9
-cost = [[INF for _ in range(n)] for _ in range(n)]
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
 
-# print(graph)
-q = deque()
+INF = int(1e9)
+cost = [[INF]*n for _ in range(n)]
+
 def bfs():
-    d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    q.append((0, 0, 0)) # 시작점 (0, 0) 바꾼횟수 0
-    while q:
-        x, y, c = q.popleft()
+    dq = deque()
+    dq.append((0, 0))
+    cost[0][0] = 0
 
-        for i in range(4):
-            new_x = x + d[i][0]
-            new_y = y + d[i][1]
-            if new_x >= 0 and new_y >= 0 and new_x < n and new_y < n:
-                if graph[new_x][new_y] == 1 and cost[new_x][new_y] > c:    # 흰방 이면서 비용이 적을 때
-                    q.append((new_x, new_y, c))
-                    cost[new_x][new_y] = c
-                elif graph[new_x][new_y] == 0 and cost[new_x][new_y] > c:   # 검은방 이면서 비용이 적을 때
-                    q.append((new_x, new_y, c+1))
-                    cost[new_x][new_y] = c+1
-                else:
-                    continue
+    d = [(-1,0),(0,1),(1,0),(0,-1)]
 
+    while dq:
+        x, y = dq.popleft()
+
+        for dx, dy in d:
+            nx, ny = x+dx, y+dy
+            if 0 <= nx < n and 0 <= ny < n:
+                # 흰방
+                if graph[nx][ny] == 1 and cost[nx][ny] > cost[x][y]:
+                    cost[nx][ny] = cost[x][y]
+                    dq.appendleft((nx, ny))
+                # 검은방
+                elif graph[nx][ny] == 0 and cost[nx][ny] > cost[x][y] + 1:
+                    cost[nx][ny] = cost[x][y] + 1
+                    dq.append((nx, ny))
 
 bfs()
-
 print(cost[n-1][n-1])
